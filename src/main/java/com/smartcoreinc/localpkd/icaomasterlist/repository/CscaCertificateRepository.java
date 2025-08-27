7195493c86318c84c111eb8882e4639d363229a8
Kyung Bae, Jung <kbjung@smartcoreinc.com>
2025-08-27 10:49:29 +0900

    update

--- src/main/java/com/smartcoreinc/localpkd/icaomasterlist/repository/CscaCertificateRepository.java
+++ src/main/java/com/smartcoreinc/localpkd/icaomasterlist/repository/CscaCertificateRepository.java
@@ -1,5 +1,7 @@
 package com.smartcoreinc.localpkd.icaomasterlist.repository;
 
+import java.util.List;
+
 import org.springframework.data.ldap.repository.LdapRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,10 @@ import com.smartcoreinc.localpkd.icaomasterlist.entity.CscaCertificate;
 
 @Repository
 public interface CscaCertificateRepository extends LdapRepository<CscaCertificate> {
+
+    /**
+     * 특정 국가 코드(countryCode)에 해당하는 모든 CSCA 인증서를 조회합니다.
+     * Spring Data LDAP의 Repository Method Query 기능을 활용합니다.
+     */
+    List<CscaCertificate> findByCountryCode(String countryCode);
 }
